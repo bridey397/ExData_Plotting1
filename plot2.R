098122dc0bf66f9fc1ceebd1893becc5f7523d4c
bridey397 <bridey397@gmail.com>
2017-06-11 08:08:38 -0400

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,9 @@
+dataFile <- "/Users/bridge23/Documents/Coursera/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+dateTime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(dateTime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
